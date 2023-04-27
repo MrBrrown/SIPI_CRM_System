a14d7a4371cf79203d6c7f4938586bbff8a0a6ee
MrBrrown <73424803+MrBrrown@users.noreply.github.com>
2023-04-27 17:30:54 +0300

    Add db connection

--- SIPI_CRM_System/Program.cs
+++ SIPI_CRM_System/Program.cs
@@ -1,6 +1,12 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using SIPI_CRM_System.Models;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<CRMdbContext>(options =>
+    options.UseMySQL(builder.Configuration.GetConnectionString("CRMdb")));
+
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
@@ -11,6 +17,13 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<CRMdbContext>();
+    context.Database.EnsureCreated();
 }
 
 app.UseHttpsRedirection();
