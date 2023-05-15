04103fccd76f8e1594bd6405a1ab9382d4de1838
MrBrrown <73424803+MrBrrown@users.noreply.github.com>
2023-05-15 12:04:50 +0300

    Add Menu Page

---
+++ SIPI_CRM_System/Pages/MenuPage/MenuPage.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SIPI_CRM_System.Pages.MenuPage
+{
+	public class MenuPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
